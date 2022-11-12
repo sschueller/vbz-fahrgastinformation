4f0decad4acc72fdd80cf0b47884e26b64d0e248
Stefan Schüller <sschueller@techdroid.com>
2022-11-12 15:49:09 +0100

    fix: repo cleanup

--- font/README.md
+++
@@ -1,7 +0,0 @@
-# Used "gbdfed" to edit font
-
-# convert for adafruit
-./bdf2adagruit.py vbz-font.bdf > vbz-font.h
-
-# convert for U8g2
-
--- server/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$file = "/tmp/vbz" . date("YmdHis") . ".json";
-
-exec('/usr/bin/node /var/www/vbz/index.js > ' . $file);
-
-$output = file_get_contents($file);
-header('Content-Type: application/json');
-echo $output;
